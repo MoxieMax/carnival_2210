d2dbf7a03151bd378be8fc759df25128223e9fff
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-14 09:24:10 -0700

    Added test for add_preference to visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,13 +3,23 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
   describe '#iteration 1' do
-    let (:visitor) {Visitor.new('Bruce', 54, '$10')}
+    
+    let (:visitor1) {Visitor.new('Bruce', 54, '$10')}
+    let (:visitor2) {Visitor.new('Tucker', 36, '$5')}
+    let (:visitor3) {Visitor.new('Penny', 64, '$15')}
+    
     it '#initialize exists and has readable attributes' do
-      expect(visitor).to be_a(Visitor)
-      expect(visitor.name).to eq('Bruce')
-      expect(visitor.height).to eq(54)
-      expect(visitor.spending_money).to eq(10)
-      expect(visitor.preferences).to eq([])
+      expect(visitor1).to be_a(Visitor)
+      expect(visitor1.name).to eq('Bruce')
+      expect(visitor1.height).to eq(54)
+      expect(visitor1.spending_money).to eq(10)
+      expect(visitor1.preferences).to eq([])
+    end
+    
+    it '#add_preference adds to an array' do
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:water)
+      expect(visitor1.preferences).to eq([:gentle, :water])
     end
   end
 end
