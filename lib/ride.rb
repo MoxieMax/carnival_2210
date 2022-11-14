db9bbe64fa9619b7fb527840eaf22a9146b74a75
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-14 09:37:02 -0700

    Created initialize in ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,12 @@
 class Ride
-
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement
+  def initialize(hash)
+    @name = hash[:name]
+    @min_height = hash[:min_height]
+    @admission_fee = hash[:admission_fee]
+    @excitement = hash[:excitement]
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,5 +4,15 @@ require './lib/ride'
 
 RSpec.describe Ride do
   describe '#iteration 2' do
+    let (:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+    it '#initialize exists and has readable attributes' do
+      expect(ride1).to be_a(Ride)
+      expect(ride1.name).to eq('Carousel')
+      expect(ride1.min_height).to eq(24)
+      expect(ride1.admission_fee).to eq(1)
+      expect(ride1.excitement).to eq(:gentle)
+    end
   end
 end
+
+# expect(ride).to eq()
