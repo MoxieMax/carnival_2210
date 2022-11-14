07b5c612f520d21c6f8ae8de67a05c88d1be5d5c
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-14 09:20:36 -0700

    Created initialize method for visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,11 +3,14 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
   describe '#iteration 1' do
-    let (:Visitor) {Visitor.new('Bruce', 54, '$10')}
+    let (:visitor) {Visitor.new('Bruce', 54, '$10')}
     it '#initialize exists and has readable attributes' do
       expect(visitor).to be_a(Visitor)
       expect(visitor.name).to eq('Bruce')
+      expect(visitor.height).to eq(54)
       expect(visitor.spending_money).to eq(10)
+      expect(visitor.preferences).to eq([])
+    end
   end
 end
 
