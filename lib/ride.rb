d1970a665c72d2440bcabe9a9f947ad9b13ea7f5
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-14 09:56:23 -0700

    Updated initialize method in ride, created test for board_rider

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,11 +2,19 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
+              :excitement,
+              :total_revenue,
+              :rider_log
   def initialize(hash)
     @name = hash[:name]
     @min_height = hash[:min_height]
     @admission_fee = hash[:admission_fee]
     @excitement = hash[:excitement]
+    @total_revenue = 0
+    @rider_log = {}
+  end
+  
+  def board_rider(visitor)
+    # @rider_log = a
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,13 +4,51 @@ require './lib/ride'
 
 RSpec.describe Ride do
   describe '#iteration 2' do
-    let (:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+    
+    let (:ride1) {Ride.new({name: 'Carousel',
+                            min_height: 24,
+                            admission_fee: 1,
+                            excitement: :gentle
+                          })}
+    let (:ride2) {Ride.new({name: 'Ferris Wheel',
+                            min_height: 36, 
+                            admission_fee: 5, 
+                            excitement: :gentle
+                          })}
+    let (:ride3) {Ride.new({name: 'Roller Coaster',
+                            min_height: 54, 
+                            admission_fee: 2,
+                            excitement: :thrilling 
+                          })}
+    
+    let (:visitor1) {Visitor.new('Bruce', 54, '$10')}
+    let (:visitor2) {Visitor.new('Tucker', 36, '$5')}
+    let (:visitor3) {Visitor.new('Penny', 64, '$15')}
+    
+    before(:each) do
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor2.add_preference(:thrilling)
+      visitor3.add_preference(:thrilling)
+    end
+    
     it '#initialize exists and has readable attributes' do
       expect(ride1).to be_a(Ride)
       expect(ride1.name).to eq('Carousel')
       expect(ride1.min_height).to eq(24)
       expect(ride1.admission_fee).to eq(1)
       expect(ride1.excitement).to eq(:gentle)
+      expect(ride1.total_revenue).to eq(0)
+      expect(ride1.rider_log).to eq({})
+      # binding.pry
+    end
+    
+    it '#board places a visitor on a ride, updates #rider_log' do
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+      
+      expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
     end
   end
 end
